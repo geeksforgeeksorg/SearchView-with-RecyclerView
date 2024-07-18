2420ed24ff9880245bca6fb86f12b5368ca73858
Ankur Nagila <ankurnaagila@gmail.com>
2024-07-18 15:14:42 +0530

    First Commit
    Add Project to the Repository

---
+++ SearchView_Android/app/src/main/java/com/geeksforgeeks/searchview_android/CourseModel.java
@@ -0,0 +1,31 @@
+package com.geeksforgeeks.searchview_android;
+
+public class CourseModel {
+    // variables for our course
+    // name and description.
+    private String courseName;
+    private String courseDescription;
+
+    // creating constructor for our variables.
+    public CourseModel(String courseName, String courseDescription) {
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+    }
+
+    // creating getter and setter methods.
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+}
